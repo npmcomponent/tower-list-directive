90f29f37d4f20f71514fabe6455d22bc7f5d9045
Lance Pollard <lancejpollard@gmail.com>
2013-05-15 03:04:28 -0700

    from todos app

--- index.js
+++ index.js
@@ -6,6 +6,7 @@
 var directive = require('tower-directive')
   , scope = require('tower-scope')
   , template = require('tower-template')
+  , Collection = require('tower-collection').Collection
   , $ = require('dom');
 
 /**
@@ -16,11 +17,21 @@ module.exports = directive('data-list', list);
 
 /**
  * List directive.
+ *
+ * 1. compute new items added that are not visible (so, have array of visible items)
+ * 2. compute which new items will be visible if inserted
+ *    (remove ones that won't be visible)
+ * 3. for each new item
+ *  - if (buffer.length) pop element from buffer, then apply scope
+ *  - else templateFn.clone(scope)
+ * 4. insert new item into DOM at correct position.
+ *    (so, basically it has a sorted collection, listening for events)
  */
 
 function list(_scope, element, attr) {
+  var self = this;
   var val = attr.value.split(/ +/);
-  element.removeAttribute(attr.name);
+  element.removeAttribute('data-list');
 
   if (val.length > 1) {
     var name = val[0];
@@ -29,31 +40,55 @@ function list(_scope, element, attr) {
     var prop = val[0];
   }
 
-  // recycled DOM elements by data item `id`
-  var elements = {};
-
   // e.g. todos
   var array = _scope.get(prop);
+  var collection;
+  if (array instanceof Collection) {
+    collection = array;
+    array = collection.toArray();
+  }
   var fn = template(element);
   var parent = element.parentNode;
   parent.removeChild(element);
-  var lastIndex = 0;
+  var id = 0;
+  var elements = {};
+
+  console.log(array);
   change(array);
 
-  _scope.on('change ' + prop, function(array){
-    change(array);
-  });
+  if (collection) {
+    //_scope.on('change ' + prop, function(array){
+    collection.on('add', function(records){
+      change(records);
+    });
 
-  function change(array) {
-    for (var i = lastIndex, n = array.length; i < n; i++) {
-      var childScope = scope(name).init({
-          parent: _scope
-        , todo: array[i].attrs
-        , i: i
-      });
+    collection.on('remove', function(records){
+      for (var i = 0, n = records.length; i < n; i++) {
+        var attrs = records[i].attrs;
+        if (elements[attrs.id]) {
+          $(elements[attrs.id]).remove(); 
+          delete elements[attrs.id];
+        }
+      }
+    });
+
+    collection.on('reset', function(records){
+      for (var key in elements) {
+        $(elements[key]).remove();
+        delete elements[key];
+      }
+      change(records);
+    }); 
+  }
+
+  function change(records) {
+    for (var i = 0, n = records.length; i < n; i++) {
+      var attrs = { parent: _scope, i: i };
+      attrs[name] = records[i];
+      var childScope = scope(name || 'anonymous').init(attrs);
       var childElement = fn.clone(childScope);
+      elements[id] = childElement;
       $(parent).prepend(childElement);
     }
-    lastIndex = n;
   }
 }
--- test/index.js
+++ test/index.js
@@ -15,4 +15,11 @@ describe('list', function(){
     assert(3 === document.querySelectorAll('#list li').length);
     assert(null === element.getAttribute('data-list'));
   });
+
+  // https://github.com/mleibman/SlickGrid/blob/master/slick.dataview.js#L809 (function refresh)
+  // https://github.com/mleibman/SlickGrid/blob/master/slick.grid.js#L1896 (function render)
+  // https://github.com/airbnb/infinity
+  it('should insert new items', function(){
+
+  });
 });
