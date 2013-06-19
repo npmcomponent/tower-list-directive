e6d1f7329d6d9b0a363bec075947cebe75af59d1
Lance Pollard <lancejpollard@gmail.com>
2013-06-19 14:32:25 -0700

    basic tracking by id

--- index.js
+++ index.js
@@ -9,27 +9,6 @@ var template = require('tower-template');
 var Collection = require('tower-collection').Collection;
 
 /**
- * Interpolation directive (XXX: remove).
- */
-
-directive('interpolation').compiler(function(el, attr){
-  var val = el.nodeValue;
-  var expressions = {};
-  val.replace(/\{\{([^\{\}]+)\}\}/g, function(_, $1){
-    // XXX: probably do more here.
-    expressions[$1] = true;
-  });
-
-  function exec(scope, el, attr) {
-    el.nodeValue = val.replace(/\{\{([^\{\}]+)\}\}/g, function(_, $1){
-      return scope.get($1);
-    });
-  }
-
-  return exec;
-});
-
-/**
  * Expose `data-list` directive.
  */
 
@@ -43,10 +22,11 @@ exports.document = 'undefined' !== typeof document && document;
 
 directive('data-each').compiler(compiler);
 
-function compiler(el, attr) {
+function compiler(templateEl, attr, nodeFn) {
   // do all this stuff up front
+  // XXX: add hoc expression, should use tower-expression.
   var val = attr.value.split(/ +/);
-  el.removeAttribute(attr.name);
+  templateEl.removeAttribute(attr.name);
 
   if (val.length > 1) {
     var name = val[0];
@@ -55,15 +35,12 @@ function compiler(el, attr) {
     var prop = val[0];
   }
 
-  var fn = template(el);
-  var parent = el.parentNode;
+  var parent = templateEl.parentNode;
   // you have to replace nodes, not remove them, to keep order.
   var comment = exports.document.createComment(' ' + attr.name + ':' + attr.value + ' ');
-  el.parentNode.replaceChild(comment, el);
-  var sourceElement = el;
-  
-  //parent.removeChild(el);
-  var prev;
+  templateEl.parentNode.replaceChild(comment, templateEl);
+  // XXX: shouldn't have to be doing this, need to figure out.
+  nodeFn = template(templateEl);
 
   /**
    * List directive.
@@ -80,23 +57,43 @@ function compiler(el, attr) {
 
   function list(scope, el, attr) {
     var cursor = el;
+    var cache = el.cache || (el.cache = {});
 
     // e.g. todos
     var array = scope.get(prop);
-    // XXX: tmp optimization
-    if (prev && prev === array) return;
-    prev = array;
     var collection;
     if (array instanceof Collection) {
       collection = array;
       array = collection.toArray();
     } // XXX: else if (isObject)
-    var id = 0;
-    var els = {};
 
+    // update DOM with [possibly] new array
     change(array);
 
-    if (collection) {
+    // XXX: todo
+    if (collection) watch(collection);
+
+    function change(records) {
+      for (var i = 0, n = records.length; i < n; i++) {
+        // XXX: should allow tracking by custom tracking function
+        // (such as by `id`), but for now just by index.
+        var id = i;
+
+        // if it's already been processed, then continue.
+        if (cache[id]) continue;
+
+        var attrs = { parent: scope, i: i };
+        attrs[name] = records[i];
+        var childScope = content(name || 'anonymous').init(attrs);
+        var childEl = templateEl.cloneNode(true);
+        cache[id] = childEl;
+        cursor.parentNode.insertBefore(childEl, cursor.nextSibling);
+        cursor = childEl;
+        nodeFn(childScope, childEl);
+      }
+    }
+
+    function watch(collection) {
       //scope.on('change ' + prop, function(array){
       collection.on('add', function(records){
         change(records);
@@ -119,19 +116,6 @@ function compiler(el, attr) {
         }
         change(records);
       }); 
-    }
-
-    function change(records) {
-      for (var i = 0, n = records.length; i < n; i++) {
-        var attrs = { parent: scope, i: i };
-        attrs[name] = records[i];
-        var childScope = content(name || 'anonymous').init(attrs);
-        var childElement = sourceElement.cloneNode(true);
-        els[id] = childElement;
-        cursor.parentNode.insertBefore(childElement, cursor.nextSibling);
-        cursor = childElement;
-        fn(childScope, childElement);
-      }
     }
   }
 
